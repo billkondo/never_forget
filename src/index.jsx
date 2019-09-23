1960808b1a98cc25f8a53cdbe24ab4c3240ce444
billkondo <willkondo@gmail.com>
2019-09-22 21:19:34 -0300

    Setup inicial

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <div>
+    <p>React here!</p>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+export default App;
